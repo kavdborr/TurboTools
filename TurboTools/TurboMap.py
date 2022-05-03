5db0707e3d395cd2ab84cfb1fc294ac555dc9666
karelvdb <karel.van.den.borre@vub.be>
2022-05-03 17:06:30 +0200

    first upload of the project in the cookiecutter template

---
+++ TurboTools/TurboMap.py
@@ -0,0 +1,301 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  1 14:30:52 2022
+
+@author: Karel Van den Borre
+"""
+
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import date
+from xml.dom import minidom
+from scipy.interpolate import interp2d
+import os
+
+class TurboMap:
+    """
+    This class contains tools to load, convert and display compressor maps.
+    """
+    name = "" # name of the map for plotting and savinf purposes
+    
+    isTurbine = False   # is this the map of a turbine?
+    isCompressor = False    # is this the map of a compressor?
+    
+    massFlow = np.zeros([10, 10])   # The map of massflow
+    compression = np.zeros([10, 10])    # map of the compression ratio
+    efficiency = np.zeros([10, 10]) # map of the adiabatic efficiency
+    
+    Kp = 1  # compression ratio scaling factor
+    Km = 1  # massflow scaling factor
+    Keta = 1    # efficiency scaling factor
+    
+    beta = np.ones([10,1])  # list of beta describing the map
+    N = np.ones([10,1]) # list of non dimensional rotational speeds
+    
+    width = 10  # width of the map (number of)
+    height = 10 # height of the map (number of rows in one field)
+    
+    surgeM = np.array([0,1])
+    surgeP = np.array([0,1])
+    
+    def __init__(self, fileLoc, width, height, isCompressor, name="default"):
+        "initialiser for the object. Populates all fields needed to display a (non-scaled) map"
+        "the width and height are user provided as the file structure is not always consistent"
+        
+        # has a custom name been specified? if so use it!
+        if not(name == "default"):
+            self.name=name
+        else:
+            # Take the name of the file without extension as default name
+            self.name = fileLoc.split(".")[-2].split("/")[-1]
+        
+        # set width and height
+        self.width=width
+        self.height=height
+        
+        # set wheter compressor or turbine
+        if isCompressor:
+            self.isCompressor=True
+        else:
+            self.isTurbine=True
+        
+        # read the map file in the gasturb format if the extention is right
+        if ".Map" in fileLoc:
+            self.readMapGT(fileLoc, width, height)
+           
+
+    def scaleMap(self, Kp, Km, Keta):
+        "function to easily scale the whole map in one line"
+        self.Kp=Kp
+        self.Km=Km
+        self.Keta=Keta
+
+    
+    def readMapGT(self, fileLoc, width, height):
+        "read the map in the Gasturb format"
+        mPR = np.zeros([0,1])   # minimum pressure ratio for turbine construction
+        MPR = np.zeros([0,1])   # maximum pressure ratio for turbine construction
+        fid = open(fileLoc)
+        while True:
+            tline = fid.readline()
+            
+            #exit condition is end of file
+            if not(tline):
+                break
+            
+            # read the mass flow map
+            if "Mass Flow" in tline:
+                self.beta, self.N, self.massFlow = self.__readSection(width, height, fid)
+                
+            # read the efficiency map
+            if "Efficiency" in tline:
+                self.beta, self.N, self.efficiency = self.__readSection(width, height, fid)  
+                
+            # read the pressure ratio map (only for compressors)
+            if ("Pressure Ratio" in tline) and not("M" in tline):
+                self.beta, self.N, self.compression = self.__readSection(width, height, fid)
+                
+            # read the min and max pressure ratios of the pressure map (only for turbines)
+            if "Min Pressure Ratio" in tline:
+                mPR = self.__readTurbineSection(fid, width, height)
+            if "Max Pressure Ratio" in tline:
+                MPR = self.__readTurbineSection(fid, width, height)
+                
+        # construct the compression ratio map in case of a turbine
+        if np.size(mPR,0)!=0:
+            self.compression=np.zeros([height, width])
+            for i in range(np.size(mPR)):
+                self.compression[i,:]=np.linspace(mPR[i], MPR[i], width)
+    
+    
+    def writeMap(self, fileLoc):
+        "write the map in the TURBO format. This makes use of xml."
+        # construct header
+        doc = minidom.Document()
+        l1 = doc.createElement("table")
+        l1.setAttribute("type", "3D")
+        l1.setAttribute("name", "Characteristic map " + self.name)
+        l1.setAttribute("description", "DESCRIPTION")
+        l1.setAttribute("version", "1.0")
+        l1.setAttribute("cdate", date.today().strftime("%d/%m/%Y"))
+        l1.setAttribute("mdate", date.today().strftime("%d/%m/%Y"))
+        l1.setAttribute("revision", "1.0")
+
+        doc.appendChild(l1)
+
+        l2 = doc.createElement("interp")
+        l2.setAttribute("default", "LINEAR")
+        l2.setAttribute("valid", "{FORBIDDEN, LINEAR}")
+        l1.appendChild(l2)
+        l3 = doc.createElement("extrap")
+        l3.setAttribute("default", "LINEAR")
+        l3.setAttribute("valid", "{FORBIDDEN, LINEAR, SPLINE}")
+        l1.appendChild(l3)
+        
+        l4 = doc.createElement("axis1")
+        l4.setAttribute("id", "Index")
+        l4.setAttribute("id", "Field indicator (-)")
+        l4.setAttribute("value", "{ 1,2,3 }")
+        l1.appendChild(l4)
+        
+        # create field for corrected speed
+        l5 = doc.createElement("axis2")
+        l5.setAttribute("id", "index")
+        l5.setAttribute("description", "Corrected speed (-)")
+        l5.setAttribute("value", np.array2string(self.N, separator=",", max_line_width=99999, precision=5, floatmode="fixed").replace("[","{").replace("]","}"))
+        l1.appendChild(l5)
+        
+        # create field for beta
+        l6 = doc.createElement("axis3")
+        l6.setAttribute("id", "Beta")
+        l6.setAttribute("description", "Beta Parameter (-)")
+        l6.setAttribute("value", np.array2string(self.beta, separator=",", max_line_width=99999, precision=5, floatmode="fixed").replace("[","{").replace("]","}"))
+        l1.appendChild(l6)
+        
+        # create field for massflow, efficiency and pressure ratio
+        l7 = doc.createElement("return")
+        l7.setAttribute("id", "Value")
+        l7.setAttribute("description", "Field value: [Index=1] Corrected mass flow rate (kg/s); [Index=2] Adiabatic efficiency (-); [Index=3] Pressure ratio t-t (-)")
+        # compose string massflowrate
+        sM = np.array2string(self.massFlow, separator=",", max_line_width=99999, precision=5, floatmode="fixed").replace("[","{").replace("]","}")
+        # compose string efficieny
+        sE = np.array2string(self.efficiency, separator=",", max_line_width=99999, precision=5, floatmode="fixed").replace("[","{").replace("]","}")
+        # compose string pressure ratio
+        sC = np.array2string(self.compression, separator=",", max_line_width=99999, precision=5, floatmode="fixed").replace("[","{").replace("]","}")
+
+        l7.setAttribute("value", "{\n" + sM + ",\n" + sE + ",\n" + sC + "\n}")
+
+        l1.appendChild(l7)
+
+        #create folder to save converted map if the folder dos not exist
+        if not os.path.exists("convertedMaps"):
+            os.mkdir("convertedMaps")
+            
+        # save files 
+        xml_str = doc.toprettyxml(indent="\t")
+        with open("convertedMaps/" + fileLoc, "w") as f:
+            f.write(xml_str)
+    
+    
+    def printMap(self):
+        "display the scaled map"
+        #always start a new figure when plotting a map
+        plt.figure()
+
+        #for compressors, plot the efficiency in terms of massflow and pressure ratio
+        if self.isCompressor:
+            plt.contourf(self.massFlow*self.Km, (self.compression-1)*self.Kp+1, self.efficiency*self.Keta, 20, cmap="gray")
+            # plot the isospeed lines
+            for i in range(np.size(self.N)):
+                plt.plot(self.massFlow[i,:]*self.Km, (self.compression[i,:]-1)*self.Kp+1, "#1c1c1c", linewidth=1)
+        # for turbine, plot efficiency in terms of massflow times rotational speed and pressure ratio
+        if self.isTurbine:
+            plt.contourf((self.massFlow.T*(self.N)).T*self.Km, (self.compression-1)*self.Kp+1, self.efficiency*self.Keta, 20, cmap="gray")
+            # plot the isospeed lines
+            for i in range(np.size(self.N)):
+                plt.plot(self.massFlow[i,:]*self.N[i]*self.Km, (self.compression[i,:]-1)*self.Kp+1, "#1c1c1c", linewidth=1)
+
+        # formatting of the plot()
+        plt.grid(True)
+        plt.title(self.name)
+        plt.ylabel(r"$\Pi$ [-]")
+        if self.isCompressor:
+            plt.xlabel(r"$\tilde{\dot{m}}$ [kg/s]")
+        else:
+            plt.xlabel(r"$\tilde{\dot{m}} \cdot{} \tilde{N}$ [kg/s]")
+        plt.colorbar(label=r"efficiency [-]")
+    
+    
+    def plotDesingPoint(self, beta, N):
+        "This funcion adds the desing point to the map"
+        # covert design point from beta and N to massflow and compression ratio
+        mf = interp2d(self.N, self.beta, self.massFlow.T)
+        pr = interp2d(self.N, self.beta, self.compression.T)
+        # plot the actual point
+        if self.isCompressor:
+            plt.plot(mf(N,beta)*self.Km, pr(N,beta)*self.Kp, 'ro', markersize=10)
+        if self.isTurbine:
+            plt.plot(mf(N,beta)*self.Km*N, pr(N,beta)*self.Kp, 'ro', markersize=10)
+    
+    
+    def plotExperiment(self, test, machine, Color="rainbow", res=50):
+        "This function plots the movement of the operating point on the map"
+        "The color of the line can vary with time id specified color is rainbow"
+        "res is the number of distinct colors"
+        # the first two are boring monochrome plots
+        if Color!="rainbow" and self.isCompressor:
+            plt.plot(test[machine+".Wcorr(Kg/s)"], test[machine+".pi(-)"], color=Color)
+        elif Color!="rainbow" and self.isTurbine:
+            plt.plot(test[machine+".Wcorr(Kg/s)"]*test[machine+".Ncorr(-)"], test[machine+".pi(-)"], color=Color)
+        # these two are gradient plots, varying the linecolor from red to green with time
+        else:
+            # min and max time to calculate color gradient
+            Tmin = test["TIME"].iloc[0]
+            Tmax = test["TIME"].iloc[-1]
+            L = test.shape[0]
+            #copy required data from pandas to numpy for efficiency
+            time = test["TIME"].to_numpy()
+            wcorr = test[machine+".Wcorr(Kg/s)"].to_numpy()
+            pi = test[machine+".pi(-)"].to_numpy()
+            # if turbine, scale massflow woth rotational speed
+            if self.isTurbine:
+                wcorr = wcorr*test[machine+".Ncorr(-)"].to_numpy()
+            for i in range(res):
+                # calculate the percentage along the experiment timeline
+                p = (time[int(i/res*L)]-Tmin)/(Tmax-Tmin)
+                # set the color in rgb values
+                Color = [1-p, p, 0.0]
+                # plot a line segment
+                plt.plot(wcorr[int((i/res)*L):int((i+1)/res*L)], pi[int((i)/res*L):int((i+1)/res*L)], color=Color)
+        
+    
+    
+    def __readSection(self, width, height, fid):
+        "function to read a block of the Gasturb text file"
+        "helper functiuon of readMapGT"
+        "function is private as it is only needed within the class"
+        totalLines = math.ceil((width+1)/5)*(height+1)
+        section = np.zeros([0])
+        for i in range(totalLines):
+            tline=fid.readline() # capture raw line
+            tline = tline.replace(",", ".") # replace comma by dot as correct decimal
+            tline = np.array(tline.split()) # divide line at whitespaces
+            tline[tline==""]=np.array([]) # replace empty cells
+            tline = tline.astype(np.float) # convert text to float
+            section = np.concatenate((section, tline))
+            
+        # beta values are the first "column"
+        beta = section[1:width+1]
+        # delete the column with betas
+        section=section[width+1:]
+        
+        # the first element of each "row" is N
+        N = section[np.arange(0,width*height,width+1)]
+        # delete the N elemets from the rows
+        section = np.delete(section, np.arange(0,width*height,width+1))
+        
+        # reshape the vector to a matrix
+        section = np.reshape(section, [int(np.size(section)/width), width])
+        
+        # returnb beta, N and the map
+        return beta, N, section
+    
+    
+    def __readTurbineSection(self, fid, width, height):
+        "reads minimum and maximum pressure ratio for turbine maps"
+        "helper function of readMapGT"
+        "function is private as it is only needed within the class"
+        # the methods are in general completely analogous to the readsection class, except with different bounds
+        totalLines = math.ceil((height+1)/5)*2
+        section = np.zeros([0])
+        for i in range(totalLines):
+            tline = fid.readline()
+            tline = tline.replace(",",".")
+            tline = np.array(tline.split())
+            tline[tline==""] = []
+            tline = tline.astype(np.float)
+            section = np.concatenate([section, tline])
+        # drop the first column, as it has useless data
+        section=section[height+2:]
+        return section
---
+++ TurboTools/main.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  1 15:33:30 2022
+
+@author: Karel Van den Borre
+
+Main file to load, print and save a map in a different format
+"""
+
+import matplotlib.pyplot as plt
+import TurboMap as tm
+import pandas as pd
+import os
+
+# close any plots which are still open
+plt.close('all')
+
+# import the experimental data. Pandas allows to access all fields by name
+# this test genertes the operating line of the machines, after an initial transient
+testResults = pd.read_csv("experiments/Full007.rpt", delimiter="\t", skiprows=4, encoding = "ISO-8859-1")
+# drop first rows to remove initial conditions experiment
+testResults=testResults.iloc[2:, :].reset_index(drop=True)
+
+# create folder to save figures
+if not os.path.exists("figures"):
+    os.mkdir("figures") 
+
+# load the high pressure compressor, scale and plot it
+HPC = tm.TurboMap("maps/HPC01_SI.Map", 20, 10, True, name="HPC")
+HPC.scaleMap(1.0000321000000001, 0.78706299099999999, 0.98847553799999999)
+HPC.writeMap("HPC01_SI.xml")
+HPC.printMap()
+HPC.plotDesingPoint(0.5, 1)
+HPC.plotExperiment(testResults, "HPC", "rainbow")
+plt.savefig("figures/HPC", dpi=300)
+
+# load the high pressure compressor, scale and plot it
+HPT = tm.TurboMap("maps/HPT01_SI.Map", 20, 5, False, name="HPT")
+HPT.scaleMap(0.90707883899999997, 0.83650503799999998, 0.97804945799999998)
+HPT.writeMap("HPT01_SI.xml")
+HPT.printMap()
+HPT.plotDesingPoint(0.5, 1)
+HPT.plotExperiment(testResults, "HPT", "rainbow")
+plt.savefig("figures/HPT", dpi=300)
+
+# Plot the net thrust in time
+plt.figure()
+plt.plot(testResults["TIME"], testResults["netThrust"]/1000)
+plt.title("Net Thrust Output")
+plt.xlabel("Time [s]")
+plt.ylabel("Thrust [kN]")
+plt.grid(True)
+plt.savefig("figures/netThrust", dpi=300)
